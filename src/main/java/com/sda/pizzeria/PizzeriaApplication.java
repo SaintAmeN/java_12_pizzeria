5144ca3c98d5643d54cfdff922e53123d10fe24c
Paweł Recław <pawel.reclaw@gmail.com>
2018-12-01 10:44:19 +0100

    Initial commit

---
+++ src/main/java/com/sda/pizzeria/PizzeriaApplication.java
@@ -0,0 +1,12 @@
+package com.sda.pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaApplication.class, args);
+	}
+}
